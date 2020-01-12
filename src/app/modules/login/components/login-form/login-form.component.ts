d42e137bb1c50cc9cf077bea355a73e5195dca66
amr.abdullah <amr.abdullah@vodafone.com>
2020-01-12 17:24:30 +0200

    feat: login form component add reactive from with validation

--- src/app/modules/login/components/login-form/login-form.component.ts
+++ src/app/modules/login/components/login-form/login-form.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/modules/shared/models/user';
+import { EMAIL_REGEX, PASSWORD_REGEX } from 'src/app/modules/shared/models/constants';
 
+/**
+ * LoginFormComponent is a component which takes user email and password
+ * from form inputs and validate them with regex
+ */
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
@@ -7,9 +14,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  @Output() login =  new EventEmitter<User>();
+
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder ) {
+  }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [ Validators.required, Validators.pattern(EMAIL_REGEX)]],
+      password: [null, [ Validators.required, Validators.pattern(PASSWORD_REGEX)]]
+    });
+  }
+
+  submit() {
+    const user: User = this.loginForm.value;
+    this.login.emit(user);
+  }
+
+  get formControls() { return this.loginForm.controls; }
+
+  get isEmailInputInvalid() {
+    const key = 'email';
+    return  this.isInputInvalid(key);
+  }
+
+
+  get isPasswordInputInvalid() {
+    const key = 'password';
+    return  this.isInputInvalid(key);
+  }
+
+  isInputInvalid(key) {
+    let isInvalid =  false;
+    if (this.formControls) {
+      const controller = this.formControls[key];
+      isInvalid = controller.invalid &&  !controller.untouched;
+    }
+    return  isInvalid;
   }
 
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [LoginComponent, LoginFormComponent],
   imports: [
     CommonModule,
+    SharedModule,
     LoginRoutingModule
   ]
 })
